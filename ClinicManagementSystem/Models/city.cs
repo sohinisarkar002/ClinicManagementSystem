c2591973580d46028a24b8033f7477bd66c2e90a
Swetaaaaaaaaaaaaaa24 <159404062+Swetaaaaaaaaaaaaaa24@users.noreply.github.com>
2024-02-08 10:17:00 +0530

    Create city.cs
    Signed-off-by: Swetaaaaaaaaaaaaaa24 <159404062+Swetaaaaaaaaaaaaaa24@users.noreply.github.com>

---
+++ ClinicManagementSystem/Models/city.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projectpart1.Models
+{
+    public class City
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
